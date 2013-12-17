2405c95fb00ed2e1abca3083d0d89d66739f6e76
Reginald Daniel <reginalddaniel924@gmail.com>
2013-12-17 16:43:39 -0500

    Finished menu and added class Class (individual clases)

--- src/main/Main.java
+++ src/main/Main.java
@@ -16,31 +16,51 @@ public class Main {
 		
 		String menu = "What type of grading scheme?\n"
 				+ "1) Point\n"
-				+ "2) Percentage ";
+				+ "2) Percentage\n"
+				+ "3) Exit\n";
 		
-		System.out.println(menu); 
-		
-		Scanner scan = new Scanner(System.in);
-		
-		int input = scan.nextInt(); 
+		while(true){
 			
-			switch(input){
+			System.out.println(menu); 
 			
-			case 1: 
-				System.out.print("You chose option 1");
-				break;
+			Scanner scan = new Scanner(System.in);
+			
+			int input = scan.nextInt(); 
 				
-			case 2:
-				System.out.print("You chose option 2");
-				break;
+				switch(input){
 				
-			default:
-				System.out.print("invalid selection"); 
+				case 1: 
+					calculatePoints(); 
+					break;
+					
+				case 2:
+					System.out.println("You chose option 2");
+					break;
+					
+				case 3:
+					System.exit(0);
+					
+				default:
+					System.out.println("invalid selection"); 
+				
+				}
 			
-			}
-			
-		
+		}//end of while
 		
 	}//end of printMenu
+	
+	public static void calculatePoints(){
+		
+		System.out.println("How many assignments do you have for this class? ");
+		
+		Scanner scan = new Scanner(System.in); 
+		
+		int numClasses = scan.nextInt(); 
+		
+		for(int i = 0; i < numClasses; i++){
+			
+		}
+		
+	}
 
-}
+}//end of class
